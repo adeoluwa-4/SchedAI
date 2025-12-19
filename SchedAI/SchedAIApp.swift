f241007559ead67739c44b2d5893b0e4116e5b20
Adeoluwa Adekoya <adeoluwaadekoya4@gmail.com>
2025-12-18 20:08:32 -0600

    Initial Commit

---
+++ SchedAI/SchedAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedAIApp.swift
+//  SchedAI
+//
+//  Created by Adeoluwa Adekoya on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SchedAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SchedAITests/SchedAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SchedAITests.swift
+//  SchedAITests
+//
+//  Created by Adeoluwa Adekoya on 12/18/25.
+//
+
+import Testing
+@testable import SchedAI
+
+struct SchedAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
