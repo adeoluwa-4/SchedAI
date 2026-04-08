1b31d224ca173b585966e886375a93a2282b3730
Adeoluwa Adekoya <adeoluwaadekoya4@gmail.com>
2026-04-07 19:44:06 -0500

    chore: update SchedAIApp.swift

--- SchedAI/SchedAIApp.swift
+++ SchedAI/SchedAIApp.swift
@@ -2,16 +2,21 @@
 //  SchedAIApp.swift
 //  SchedAI
 //
-//  Created by Adeoluwa Adekoya on 12/18/25.
+//  Created by Adeoluwa Adekoya on 9/5/25.
 //
 
 import SwiftUI
 
 @main
 struct SchedAIApp: App {
+    @StateObject private var app = AppState()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LogoLaunchView()
+                .environmentObject(app)
+                // Default = System. If the user overrides in Settings, app.theme changes it.
+                .preferredColorScheme(app.theme.colorScheme)
         }
     }
 }
