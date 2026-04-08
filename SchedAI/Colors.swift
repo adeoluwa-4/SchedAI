3837bb89199e39260e9537aa82bc181aa0d81d77
Adeoluwa Adekoya <adeoluwaadekoya4@gmail.com>
2026-04-07 19:44:06 -0500

    chore: update Colors.swift

---
+++ SchedAI/Colors.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+extension Color {
+    /// Consistent dark blue used for brand icons and accents
+    static let brandBlue = Color(red: 0.0, green: 0.32, blue: 0.80) // #0052CC-ish
+}
