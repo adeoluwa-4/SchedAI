2bded907a5146115c838922c83afd954d612f854
Adeoluwa Adekoya <adeoluwaadekoya4@gmail.com>
2026-05-12 23:09:10 -0500

    app: centralize legal and support links

---
+++ SchedAI/LegalLinks.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum LegalLinks {
+    static let privacy = URL(string: "https://schedai-snowy.vercel.app/privacy/")!
+    static let terms = URL(string: "https://schedai-snowy.vercel.app/terms/")!
+    static let support = URL(string: "https://schedai-snowy.vercel.app/support/")!
+    static let privacyChoices = URL(string: "https://schedai-snowy.vercel.app/privacy-choices/")!
+}
